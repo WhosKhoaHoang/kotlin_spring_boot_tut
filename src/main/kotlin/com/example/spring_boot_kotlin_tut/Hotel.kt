161c70527f4ae39ad822211d9fbb0b7461ea2340
Khoa Hoang <whoskhoahoang@gmail.com>
2020-06-17 15:59:33 +0900

    Define Hotel entity

---
+++ src/main/kotlin/com/example/spring_boot_kotlin_tut/Hotel.kt
@@ -0,0 +1,34 @@
+package com.example.spring_boot_kotlin_tut
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+
+// Note that an "Entity" represents a table stored in a database
+@Entity
+class Hotel(val name: String, val classification: Int, val numRooms: Int) {
+    // When we declare a variable with the "val" keyword, then that
+    // indicates an immutable variable (works like the "final" keyword
+    // in Java). "var" is the opposite of "val".
+
+    // Like in python, you don't explicitly need to define
+    // getters and setters for these instance variables
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    var id: Long = 0
+    var numFreeRooms: Int = numRooms
+
+    fun checkInGuests(numGuests: Int) {
+        if (this.numFreeRooms >= numGuests) {
+            this.numFreeRooms -= numGuests
+        }
+    }
+
+    fun checkOut(numGuests: Int) {
+        this.numFreeRooms += numGuests
+    }
+
+
+}
